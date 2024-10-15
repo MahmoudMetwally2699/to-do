f9caa6f60f66a8cf574f264e91c71aa32dc7bccf
MahmoudMetwally2699 <mahmmetwally26@gmail.com>
2024-10-15 09:19:58 +0300

    Rename 'servers' parameter to 'server' for clarity in useClientOnlyValue function

--- components/useClientOnlyValue.ts
+++ components/useClientOnlyValue.ts
@@ -1,5 +1,5 @@
 // This function is web-only as native doesn't currently support server (or build-time) rendering.
-export function useClientOnlyValue<S, C>(servers: S, clients: C): S | C {
+export function useClientOnlyValue<S, C>(server: S, clients: C): S | C {
   return clients;
 }
 //dsfih
